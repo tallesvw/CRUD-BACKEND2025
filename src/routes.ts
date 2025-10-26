cf8abe635b7fc7822c196f17fcbc5af44807c0da
Talles <talizimvw@outlook.com>
2025-10-26 16:53:47 -0300

    primeiro commit

---
+++ src/routes.ts
@@ -0,0 +1,8 @@
+import {Router, Request, Response} from "express";
+
+
+const router = Router();
+router.get("/test", (request: Request, response: Response) =>{
+return response.json({ ok: true });
+});
+export { router };
