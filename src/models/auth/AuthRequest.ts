449f3ac36c6752125ad3575ed986745a28d3bd2c
Talles <talizimvw@outlook.com>
2025-10-28 07:18:31 -0300

    implementacao do jwt

---
+++ src/models/auth/AuthRequest.ts
@@ -0,0 +1,5 @@
+export interface AuthRequest {
+    user: String;
+    password: String;
+
+}
---
+++ src/services/users/AuthUserService.ts
@@ -0,0 +1,52 @@
+import { compare } from "bcryptjs";
+import { sign } from "jsonwebtoken";
+import PrismaClient from "../../prisma/index.js"
+import  {} from "../../models/auth/AuthRequest.js"
+import { AuthRequest } from '../../models/auth/AuthRequest.js';
+import prismaClient from "../../prisma/index.js";
+
+
+class AuthUserService {
+    async execute({user, password}: AuthRequest) {
+        if(!user) {
+            throw new Error("Usuário Precisa ser preenchido!");   
+        }
+
+        if(!password) {
+            throw new Error("Senha Precisa ser preenchido!");   
+        }
+        const register = await prismaClient.user.findFirst({
+            where: {
+                user: user
+            }
+        });
+        if(!register) {
+            throw new Error("Usuário ou senha incorreto!");
+        }
+        const passwordMatch = await compare(password, register?.password)
+        if(!passwordMatch) {
+            throw new Error("Senha Errada!");
+        }
+
+        const token = sign( 
+            {
+                user: register?.user,
+                password: register?.password 
+            },
+            process.env.JWT_SECRET as string,
+            {
+                subject: user?.id,
+                expiresIn: "8h"
+            }
+        );
+
+        return {
+            id:register?.id,
+            name: register?.user,
+            token: token
+        }
+
+    }
+}
+
+export { AuthUserService }
